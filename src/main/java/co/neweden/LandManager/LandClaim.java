e38034f90b9e087e79f7337602f24b6885ed2485
aaron <AaronMcHale>
2016-11-15 22:28:26 +0000

    LandClaim: Added missing setter methods for existing getter methods

--- src/main/java/co/neweden/LandManager/LandClaim.java
+++ src/main/java/co/neweden/LandManager/LandClaim.java
@@ -24,6 +24,19 @@ public class LandClaim extends ACL {
         this.id = id;
     }
 
+    private boolean setDBValue(String key, String value) {
+        try {
+            PreparedStatement st = LandManager.db.prepareStatement("UPDATE landclaims SET " + key + "=? WHERE land_id=?;");
+            st.setString(1, value);
+            st.setInt(2, id);
+            st.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            LandManager.getPlugin().getLogger().log(java.util.logging.Level.SEVERE, "Land Claim #" + id + ": An SQL Exception occurred while trying to update " + key + " to \"" + value + "\"");
+            return false;
+        }
+    }
+
     public String getDisplayName() {
         if (displayName == null)
             return "Land Claim #" + id;
@@ -31,9 +44,25 @@ public class LandClaim extends ACL {
             return displayName;
     }
 
+    public boolean setDisplayName(String name) {
+        if (setDBValue("displayName", name)) {
+            displayName = name;
+            return true;
+        } else
+            return false;
+    }
+
     public int getID() { return id; }
 
     public UUID getOwner() { return owner; }
+
+    public boolean setOwner(UUID uuid) {
+        if (setDBValue("owner", uuid.toString())) {
+            owner = uuid;
+            return true;
+        } else
+            return false;
+    }
 
     public Level getEveryoneAccessLevel() {
         if (everyoneAccessLevel == null)
@@ -42,9 +71,33 @@ public class LandClaim extends ACL {
             return everyoneAccessLevel;
     }
 
+    public boolean setEveryoneAccessLevel(Level level) {
+        if (setDBValue("everyone_acl_level", level.toString())) {
+            everyoneAccessLevel = level;
+            return true;
+        } else
+            return false;
+    }
+
     public Location getHomeLocation() { return homeLocation; }
 
+    public boolean setHomeLocation(Location homeLocation) {
+        if (setDBValue("home_location", Util.locationToString(homeLocation))) {
+            this.homeLocation = homeLocation;
+            return true;
+        } else
+            return false;
+    }
+
     public Material getIconMaterial() { return iconMaterial; }
+
+    public boolean setIconMaterial(Material material) {
+        if (setDBValue("icon_material", material.toString())) {
+            iconMaterial = material;
+            return true;
+        } else
+            return false;
+    }
 
     public boolean claimChunk(Chunk chunk) {
         if (LandManager.isChunkClaimed(chunk))
