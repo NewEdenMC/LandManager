5bedae91bfdd0c452a55b93349587e7b84ee1836
aaron <AaronMcHale>
2016-11-23 20:50:49 +0000

    Liquid won't flow into Land Claims, explosions crossing Land borders are cancelled

--- src/main/java/co/neweden/LandManager/Listeners/BlockEvents.java
+++ src/main/java/co/neweden/LandManager/Listeners/BlockEvents.java
@@ -1,16 +1,49 @@
 package co.neweden.LandManager.Listeners;
 
+import co.neweden.LandManager.LandClaim;
+import co.neweden.LandManager.LandManager;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.*;
+import org.bukkit.event.entity.EntityExplodeEvent;
+
+import java.util.Collection;
+import java.util.HashSet;
 
 public class BlockEvents implements Listener {
 
-    @EventHandler (ignoreCancelled = true, priority = EventPriority.HIGH)
-    public void onBlockBurn(BlockBurnEvent event) {
-
+    private void handleBlockMove(Cancellable event, Block from, Block to) {
+        LandClaim fromLand = LandManager.getLandClaim(from.getChunk());
+        LandClaim toLand = LandManager.getLandClaim(to.getChunk());
+        if (toLand == null) return;
+        if (toLand.equals(fromLand)) return;
+        event.setCancelled(true);
     }
+
+    @EventHandler (ignoreCancelled = true, priority = EventPriority.HIGH)
+    public void onBlockFromTo(BlockFromToEvent event) { handleBlockMove(event, event.getBlock(), event.getToBlock()); }
+
+    private void handleBlockExplosion(Cancellable event, Collection<Block> blockList) {
+        Collection<LandClaim> land = new HashSet<>();
+        blockList.stream().forEach(
+                e -> land.add(LandManager.getLandClaim(e.getChunk()))
+        );
+        System.out.print("list: " + land);
+        if (land.size() > 1)
+            event.setCancelled(true);
+    }
+
+    @EventHandler (ignoreCancelled = true, priority = EventPriority.HIGH)
+    public void onBlockExplode(BlockExplodeEvent event) { handleBlockExplosion(event, event.blockList()); }
+
+    @EventHandler (ignoreCancelled = true, priority = EventPriority.HIGH)
+    public void onEntityExplode(EntityExplodeEvent event) { handleBlockExplosion(event, event.blockList()); }
 
     @EventHandler (ignoreCancelled = true, priority = EventPriority.HIGH)
     public void onBlockPistonRetract(BlockPistonRetractEvent event) {
