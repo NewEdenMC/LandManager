f7073fe82846246417f13f05024286e537696fc6
aaron <AaronMcHale>
2017-01-21 13:26:59 +0000

    Implemented protection command handling, added /pinfo command

--- src/main/java/co/neweden/LandManager/Commands/ProtectionCommands.java
+++ src/main/java/co/neweden/LandManager/Commands/ProtectionCommands.java
@@ -1,24 +1,33 @@
 package co.neweden.LandManager.Commands;
 
+import co.neweden.LandManager.ACL;
 import co.neweden.LandManager.LandManager;
+import co.neweden.LandManager.Protection;
 import co.neweden.LandManager.Util;
+import org.bukkit.Bukkit;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandException;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.*;
 
-public class ProtectionCommands implements CommandExecutor {
+public class ProtectionCommands implements CommandExecutor, Listener {
 
     private Map<Player, CommandCache> cmdCache = new HashMap<>();
     private Collection<Player> persistPlayers = new HashSet<>();
 
     public ProtectionCommands() {
         LandManager.getPlugin().getCommand("ppersist").setExecutor(this);
+        LandManager.getPlugin().getCommand("pinfo").setExecutor(this);
+        Bukkit.getPluginManager().registerEvents(this, LandManager.getPlugin());
     }
 
     public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
@@ -30,6 +39,7 @@ public class ProtectionCommands implements CommandExecutor {
         Player player = (Player) sender;
         if (handlePersist(command, player)) return true; // we are done if the player ran the persist command
         cmdCache.put(player, new CommandCache(command.getName(), args, System.currentTimeMillis()));
+        sender.sendMessage(Util.formatString("&aThe command is ready, left click a block to run this command!"));
 
         return true;
     }
@@ -48,6 +58,53 @@ public class ProtectionCommands implements CommandExecutor {
             ));
         }
         return true;
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        if (!event.getAction().equals(Action.LEFT_CLICK_BLOCK) ||
+                !cmdCache.containsKey(player)) return;
+
+        CommandCache cmd = cmdCache.get(player);
+        if (!persistPlayers.contains(player))
+            cmdCache.remove(player);
+
+        try {
+            switch (cmd.command.toLowerCase()) {
+                case "pinfo": infoCommand(player, event.getClickedBlock()); break;
+            }
+        } catch (CommandException e) {
+            event.getPlayer().sendMessage(Util.formatString(e.getMessage()));
+        }
+    }
+
+    private void infoCommand(Player player, Block block) {
+        String status = "&7Not Protected";
+        String owner = "";
+        String acl = "- &7EVERYONE (FULL_ACCESS)";
+
+        Protection protection = LandManager.getProtection(block);
+        if (protection != null) {
+            if (!protection.testAccessLevel(player, ACL.Level.VIEW, "landmanager.pinfo.any"))
+                throw new CommandException("&cThis is protected but you do not have permission to view the protection Info.");
+
+            status = "&aProtected";
+            owner = Bukkit.getOfflinePlayer(protection.getOwner()).getName();
+            acl = "";
+            for (Map.Entry<UUID, ACL.Level> entry : protection.getACL().entrySet()) {
+                if (entry.getKey() != null)
+                    acl += "- " + Bukkit.getOfflinePlayer(entry.getKey()).getName() + " (" + entry.getValue() + ")\n";
+                else
+                    acl += "- &7EVERYONE (" + entry.getValue() + ")&r\n";
+            }
+        }
+
+        player.sendMessage(Util.formatString(
+                "Chunk status: " + status + "&r\n" +
+                "Land owned by: " + owner + "&r\n\n" +
+                "Access Control List:\n" + acl
+        ));
     }
 
 }
