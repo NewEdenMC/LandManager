8cba8353f27fec7b68c1db375a2f95689a14c72d
aaron <AaronMcHale>
2017-02-24 22:12:19 +0000

    For InventoryHolder Protections Players with VIEW Access can open but not interact

--- src/main/java/co/neweden/LandManager/Listeners/InteractEvents.java
+++ src/main/java/co/neweden/LandManager/Listeners/InteractEvents.java
@@ -3,18 +3,22 @@ package co.neweden.LandManager.Listeners;
 import co.neweden.LandManager.*;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.block.BlockState;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerShearEntityEvent;
 import org.bukkit.event.vehicle.VehicleDestroyEvent;
 import org.bukkit.event.vehicle.VehicleEnterEvent;
+import org.bukkit.inventory.InventoryHolder;
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -26,6 +30,9 @@ public class InteractEvents implements Listener {
     }
 
     private void handleEvent(Location targetLocation, Cancellable event, Entity callingEntity) {
+        handleEvent(targetLocation, event, callingEntity, ACL.Level.INTERACT, "interactany");
+    }
+    private void handleEvent(Location targetLocation, Cancellable event, Entity callingEntity, ACL.Level needed, String bypassPermSuffix) {
         ACL acl = LandManager.getFirstACL(targetLocation);
         if (acl == null || callingEntity == null) return; // callingEntity may sometimes be null
         if (!(callingEntity instanceof Player)) return;
@@ -34,16 +41,15 @@ public class InteractEvents implements Listener {
         String bperm = "";
         String typeName = "";
         if (acl instanceof Protection) {
-            bperm = "landmanager.protection.interactany";
+            bperm = "landmanager.protection." + bypassPermSuffix;
             typeName = "Protection";
         }
         if (acl instanceof LandClaim) {
-            bperm = "landmanager.land.interactany";
+            bperm = "landmanager.land." + bypassPermSuffix;
             typeName = "Land Claim";
         }
 
-        if (acl.testAccessLevel(player, ACL.Level.INTERACT, bperm))
-            return;
+        if (acl.testAccessLevel(player, needed, bperm)) return;
 
         event.setCancelled(true);
         player.sendMessage(Util.formatString("&cYou do not have permission to interact with this " + typeName));
@@ -52,7 +58,15 @@ public class InteractEvents implements Listener {
 
     @EventHandler (ignoreCancelled = true, priority = EventPriority.HIGH)
     public void onPlayerInteract(PlayerInteractEvent event) {
-        handleEvent(event.getClickedBlock().getLocation(), event, event.getPlayer());
+        ACL.Level needed = ACL.Level.INTERACT;
+        String bypassPermSuffix = "interactany";
+
+        if (event.getClickedBlock().getState() instanceof InventoryHolder && event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+            needed = ACL.Level.VIEW;
+            bypassPermSuffix = "viewany";
+        }
+
+        handleEvent(event.getClickedBlock().getLocation(), event, event.getPlayer(), needed, bypassPermSuffix);
     }
 
     @EventHandler (ignoreCancelled = true, priority = EventPriority.HIGH)
@@ -78,6 +92,20 @@ public class InteractEvents implements Listener {
     @EventHandler (ignoreCancelled = true, priority = EventPriority.HIGH)
     public void onSignChange(SignChangeEvent event) {
         handleEvent(event.getBlock().getLocation(), event, event.getPlayer());
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+    public void onInventroyClick(InventoryClickEvent event) {
+        InventoryHolder ih = event.getInventory().getHolder();
+        if (!(ih instanceof BlockState)) return;
+
+        Protection p = LandManager.protections().get(((BlockState) ih).getBlock());
+        if (p == null) return;
+
+        if (p.testAccessLevel(event.getWhoClicked(), ACL.Level.INTERACT, "landmanager.protection.interactany")) return;
+
+        event.getWhoClicked().sendMessage(Util.formatString("&cYou do not permission to interact with this Protection"));
+        event.setCancelled(true);
     }
 
 }
