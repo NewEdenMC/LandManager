31af3df6ae0aaa78640d384912ab00473e469e04
aaron <AaronMcHale>
2017-02-25 16:21:57 +0000

    Reverted to caching Protections when they are required instead of on Chunk Load, will be more robust

--- src/main/java/co/neweden/LandManager/Protections.java
+++ src/main/java/co/neweden/LandManager/Protections.java
@@ -6,7 +6,6 @@ import org.bukkit.block.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.event.world.ChunkUnloadEvent;
 
 import java.sql.PreparedStatement;
@@ -32,21 +31,66 @@ public class Protections implements Listener {
     public boolean isWorldRestricted(World world) { return LandManager.isWorldRestricted("protections", world); }
 
     public BlockProtection get(Block block) {
+        // Setup b1 and b2, in general b1 and b2 will be the same except where "block" is e.g. a Chest or Door
         final Block b1 = block;
         Block joining = Util.getJoiningBlock(block);
         final Block b2 = (joining != null) ? joining : block;
+
+        // Search currently loaded protections to see if protection for "block" is already loaded, if so return it
         Optional<BlockProtection> bpOption = blockProtections.stream()
                 .filter(p -> p.getBlock().equals(b1) || p.getBlock().equals(b2)).findFirst();
-        return bpOption.orElse(null);
+        if (bpOption.isPresent())
+            return bpOption.get();
+
+        // If we couldn't find the relevant protection query the database to see if it's there
+        try {
+            PreparedStatement st = LandManager.getDB().prepareStatement("SELECT * FROM protections WHERE (world=? AND x=? AND y=? AND z=?) OR (world=? AND x=? AND y=? AND z=?)");
+
+            st.setString(1, b1.getWorld().getName());
+            st.setInt(2, b1.getX()); st.setInt(3, b1.getY()); st.setInt(4, b1.getZ());
+
+            st.setString(5, b2.getWorld().getName());
+            st.setInt(6, b2.getX()); st.setInt(7, b2.getY()); st.setInt(8, b2.getZ());
+
+            ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                BlockProtection p = generate(rs);
+                if (p == null) return null; // If an error occurred while generating, skip this one, console output already sent
+                blockProtections.add(p);
+                return p;
+            }
+        } catch (SQLException e) {
+            LandManager.getPlugin().getLogger().log(Level.SEVERE, "An SQL Exception occurred while trying to get Protection data for block at: x=" + block.getX() + " y=" + block.getType() + " z=" + block.getZ(), e);
+            return null;
+        }
+
+        return null;
     }
 
-    public Collection<Protection> getSurroundingProtections(Block block) {
-        return blockProtections.stream()
-                .filter(e ->
-                            (block.getX() - 1 == e.getBlock().getX() || block.getX() == e.getBlock().getX() || block.getX() + 1 == e.getBlock().getX()) &&
-                            (block.getY() - 1 == e.getBlock().getY() || block.getY() == e.getBlock().getY() || block.getY() + 1 == e.getBlock().getY()) &&
-                            (block.getZ() - 1 == e.getBlock().getZ() || block.getZ() == e.getBlock().getZ() || block.getZ() + 1 == e.getBlock().getZ())
-                ).collect(Collectors.toSet());
+    private BlockProtection generate(ResultSet rs) throws SQLException {
+        BlockProtection p = new BlockProtection(rs.getInt("protection_id"));
+
+        try {
+            p.owner = UUID.fromString(rs.getString("owner"));
+        } catch (IllegalArgumentException e) {
+            LandManager.getPlugin().getLogger().log(Level.SEVERE, "Protection ID #" + rs.getInt("protection_id") + ": UUID \"" + rs.getString("owner") + "\" is not valid, Protection will not be loaded.");
+            return null;
+        }
+
+        try {
+            if (rs.getString("everyone_acl_level") != null)
+                p.everyoneAccessLevel = ACL.Level.valueOf(rs.getString("everyone_acl_level"));
+        } catch (IllegalArgumentException e) {
+            LandManager.getPlugin().getLogger().log(Level.SEVERE, "Protection ID #" + rs.getInt("protection_id") + ": Value \"" + rs.getString("everyone_acl_level") + "\" is not valid, default Level will be used instead.");
+        }
+
+        World w = Bukkit.getWorld(rs.getString("world"));
+        double x = rs.getDouble("x");
+        double y = rs.getDouble("y");
+        double z = rs.getDouble("z");
+        p.block = new Location(w, x, y, z).getBlock();
+
+        return p;
     }
 
     public BlockProtection create(UUID owner, Block block) throws RestrictedWorldException {
@@ -54,13 +98,12 @@ public class Protections implements Listener {
             throw new RestrictedWorldException(block.getWorld(), "Unable to create Protection in this World as it is restricted by the configuration.", "You are not allowed to create a protection in this world.");
 
         try {
-            PreparedStatement st = LandManager.getDB().prepareStatement("INSERT INTO `protections` (`world`, `chunk_loc`, `x`, `y`, `z`, `owner`) VALUES (?, ?, ?, ?, ?, ?);", Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement st = LandManager.getDB().prepareStatement("INSERT INTO `protections` (`world`, `x`, `y`, `z`, `owner`) VALUES (?, ?, ?, ?, ?);", Statement.RETURN_GENERATED_KEYS);
             st.setString(1, block.getWorld().getName());
-            st.setString(2, block.getChunk().getX() + ":" + block.getChunk().getZ());
-            st.setInt(3, block.getX());
-            st.setInt(4, block.getY());
-            st.setInt(5, block.getZ());
-            st.setString(6, owner.toString());
+            st.setInt(2, block.getX());
+            st.setInt(3, block.getY());
+            st.setInt(4, block.getZ());
+            st.setString(5, owner.toString());
             st.executeUpdate();
             ResultSet rs = st.getGeneratedKeys();
             rs.next();
@@ -96,56 +139,6 @@ public class Protections implements Listener {
         return true;
     }
 
-    public boolean loadAllForChunk(Chunk chunk) {
-        String cloc = chunk.getX() + ":" + chunk.getZ();
-        try {
-            PreparedStatement st = LandManager.getDB().prepareStatement("SELECT * FROM protections WHERE world=? AND chunk_loc=?");
-            st.setString(1, chunk.getWorld().getName());
-            st.setString(2, cloc);
-            ResultSet rs = st.executeQuery();
-            while (rs.next()) {
-                int protection_id = rs.getInt("protection_id");
-                if (blockProtections.stream().filter(e -> e.getID() == protection_id).count() > 0)
-                    continue; // If the current protection is already loaded skip it
-
-                BlockProtection p = generate(rs);
-                if (p == null) continue; // If an error occurred while generating, skip this one, console output already sent
-
-                blockProtections.add(p);
-            }
-        } catch (SQLException e) {
-            LandManager.getPlugin().getLogger().log(Level.SEVERE, "An SQL Exception occurred while trying to get Protection data for chunk_loc: " + cloc, e);
-            return false;
-        }
-        return true;
-    }
-
-    private BlockProtection generate(ResultSet rs) throws SQLException {
-        BlockProtection p = new BlockProtection(rs.getInt("protection_id"));
-
-        try {
-            p.owner = UUID.fromString(rs.getString("owner"));
-        } catch (IllegalArgumentException e) {
-            LandManager.getPlugin().getLogger().log(Level.SEVERE, "Protection ID #" + rs.getInt("protection_id") + ": UUID \"" + rs.getString("owner") + "\" is not valid, Protection will not be loaded.");
-            return null;
-        }
-
-        try {
-            if (rs.getString("everyone_acl_level") != null)
-                p.everyoneAccessLevel = ACL.Level.valueOf(rs.getString("everyone_acl_level"));
-        } catch (IllegalArgumentException e) {
-            LandManager.getPlugin().getLogger().log(Level.SEVERE, "Protection ID #" + rs.getInt("protection_id") + ": Value \"" + rs.getString("everyone_acl_level") + "\" is not valid, default Level will be used instead.");
-        }
-
-        World w = Bukkit.getWorld(rs.getString("world"));
-        double x = rs.getDouble("x");
-        double y = rs.getDouble("y");
-        double z = rs.getDouble("z");
-        p.block = new Location(w, x, y, z).getBlock();
-
-        return p;
-    }
-
     public boolean canBlockBeProtected(Material material) {
         if (!LandManager.getPlugin().getConfig().isConfigurationSection("protections.blocks") || // if no blocks have been configured any blocks can be protected
                 LandManager.getPlugin().getConfig().isConfigurationSection("protections.blocks." + material.toString().toLowerCase())) return true;
@@ -155,11 +148,6 @@ public class Protections implements Listener {
 
     public boolean canBlockAutoProtect(Material material) {
         return LandManager.getPlugin().getConfig().getBoolean("protections.blocks." + material.toString().toLowerCase() + ".auto_protect", false);
-    }
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onChunkLoad(ChunkLoadEvent event) {
-        loadAllForChunk(event.getChunk());
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
